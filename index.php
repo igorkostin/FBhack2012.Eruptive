44a6b5de917b67fdad19c445c700bd1692b81389
Igor Kostin <igor@icsoft.ru>
2012-09-20 14:29:39 -0400

    new2

--- index.php
+++ index.php
@@ -1,10 +1,36 @@
+<div id="fb-root"></div>
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '346459112112091', // App ID
+      channelUrl : 'http://fbhackeruptive2.icsoft.ca/channel.php', // Channel File
+      status     : true, // check login status
+      cookie     : true, // enable cookies to allow the server to access the session
+      xfbml      : true  // parse XFBML
+    });
 
-Hello World!
+    // Additional initialization code here
+  };
 
-it''s my first file created in cloud9 environment!
+  // Load the SDK Asynchronously
+  (function(d){
+     var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement('script'); js.id = id; js.async = true;
+     js.src = "//connect.facebook.net/en_US/all.js";
+     ref.parentNode.insertBefore(js, ref);
+   }(document));
+</script>
+
 
 <?php
 
     echo "seems to be coool though test test test";
     
 ?>
+
+<script>
+FB.api('/me', function(response) {
+  alert('Your name is ' + response.name);
+});
+</script>
