6d29ba7180f30e08e47b2e44c2caef48cba99152
libra guest <igor@icsoft.ru>
2012-09-20 00:02:27 -0400

    sdfsd

---
+++ index.php
@@ -0,0 +1,4 @@
+
+Hello World!
+
+it's my first file created in cloud9 environment!
---
+++ test.php
@@ -0,0 +1,9 @@
+<?php
+
+    //echo php_info();
+    echo "sdfasdfasdf\n============\n";
+    
+    $r = array( '1'=>'2' );
+    count($r);
+
+?>
