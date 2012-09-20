5313df6a8147f18a1ff8656082ad73869cede6e9
Igor Kostin <igor@icsoft.ru>
2012-09-20 12:44:23 -0400

    my awesome comment from FBworldHack

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 
 Hello World!
 
-it's my first file created in cloud9 environment!
+it''s my first file created in cloud9 environment!
+
+<?php
+
+    echo "seems to be coool though test test test";
+    
+?>
